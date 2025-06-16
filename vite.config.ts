c943362a3ca20f686ceace82d0b22547790a7d64
Automated Uploader Script <auto-uploader@example.com>
2025-06-16 14:49:13 +0300

    Auto-update from script 'nestor_ftb_01(11).zip' at Δευ 16/06/2025 14:49:13,48

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 
-
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import { fileURLToPath } from 'url'; // Added for __dirname replacement
@@ -21,7 +20,8 @@ export default defineConfig(({ mode }) => {
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
-        }
+        },
+        dedupe: ['immer'] // Added to ensure consistent 'immer' resolution
       }
     };
 });
